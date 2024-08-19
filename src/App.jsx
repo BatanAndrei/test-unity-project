da69c8f6f2a1f3e300425953bc67b0dbd2efdfdc
BatanAndrei <yabatan777@gmail.com>
2024-08-19 10:40:37 +0300

    init: create project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import MagicUnityGame from '../src/components/MagicUnityGame/MagicUnityGame';
+
+function App() {
+	return (
+		<div className="App">
+			<h1>Наш сайт</h1>
+			<MagicUnityGame/>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/MagicUnityGame/MagicUnityGame.jsx
@@ -0,0 +1,12 @@
+const MagicUnityGame = () => {
+    return (
+        <iframe 
+        src="https://batanandrei.github.io/magic-build-unity/"
+        title="Magic unity game"
+        width="1000"
+        height="650"
+        ></iframe>
+    )
+};
+
+export default MagicUnityGame;
