beaf91597307eacd956e8e94de5cd3868efe62d7
BatanAndrei <yabatan777@gmail.com>
2024-08-23 20:17:26 +0300

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
-import MagicUnityGame from '../src/components/MagicUnityGame/MagicUnityGame';
+//import MagicUnityGame from '../src/components/MagicUnityGame/MagicUnityGame';
+
 
 function App() {
 	return (
 		<div className="App">
 			<h1>Наш сайт</h1>
-			<MagicUnityGame/>
+			<a href='https://batanandrei.github.io/bird-build-unity/' target="_blank">ИГРА</a>
+			{/* <MagicUnityGame/> */}
 		</div>
 	);
 }
